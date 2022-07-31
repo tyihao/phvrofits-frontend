03f75f95ce34127503ad57d13246c367dedf3fd7
tyihao <yihaotan1996@gmail.com>
2022-07-31 19:56:03 +0800

    Updates

---
+++ precache-manifest.992ea9a66e15b282a3c5e7f0bbd96732.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4df7ef85e8952587db04",
+    "url": "/phvrofits-frontend/static/css/main.f64d8969.chunk.css"
+  },
+  {
+    "revision": "4df7ef85e8952587db04",
+    "url": "/phvrofits-frontend/static/js/main.9babdee3.chunk.js"
+  },
+  {
+    "revision": "534652efdc0180a8b871",
+    "url": "/phvrofits-frontend/static/js/runtime~main.b482606a.js"
+  },
+  {
+    "revision": "8f31ea609661c417a8e3",
+    "url": "/phvrofits-frontend/static/js/2.ede0cddf.chunk.js"
+  },
+  {
+    "revision": "2a7670296c5982365b73c68c3953934c",
+    "url": "/phvrofits-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/phvrofits-frontend/precache-manifest.ab145d35c4bf97bf8d7fb600b2130832.js"
+  "/phvrofits-frontend/precache-manifest.992ea9a66e15b282a3c5e7f0bbd96732.js"
 );
 
 workbox.clientsClaim();
