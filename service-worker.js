e00f7b849d2e7b0c267375500e43b13a0e4cfcf2
Tan Yi Hao <yi_hao_tan@hotmail.com>
2022-10-21 03:36:35 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/phvrofits-frontend/precache-manifest.fa010d2524040704a578a65099926594.js"
+  "/phvrofits-frontend/precache-manifest.564990743fadcb6620fe209ac6538e49.js"
 );
 
 workbox.clientsClaim();
