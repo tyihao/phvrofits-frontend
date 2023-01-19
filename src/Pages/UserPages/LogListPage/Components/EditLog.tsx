78cc0f54d5752192e098d586b4d5d3a4abbe3a63
tyihao <yihaotan1996@gmail.com>
2023-01-19 15:20:53 +0800

    refactored LogListPage.tsx

---
+++ src/Pages/UserPages/LogListPage/Components/EditLog.tsx
@@ -0,0 +1,200 @@
+import CloseIcon from '@mui/icons-material/Close';
+import {
+  AppBar,
+  Box,
+  Button,
+  Dialog,
+  Grid,
+  InputAdornment,
+  TextField,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import { useEffect, useState } from 'react';
+import 'react-day-picker/dist/style.css';
+import { editEntryOnFirebase } from '../../../../Firebase';
+import { LogInfo } from '../../../../Utils/types';
+import Transition from './Transition';
+
+interface EditLogProps {
+  handleClose: (currentLog?: LogInfo) => void;
+  openDialog: boolean;
+  selectedLog: LogInfo;
+  confirmEdit: (log: LogInfo) => void;
+}
+
+const EditLog = (props: EditLogProps) => {
+  const { openDialog, handleClose, confirmEdit, selectedLog } = props;
+  const [gojekEarnings, setGojekEarnings] = useState<string>('');
+  const [grabEarnings, setGrabEarnings] = useState<string>('');
+  const [tadaEarnings, setTadaEarnings] = useState<string>('');
+  const [rydeEarnings, setRydeEarnings] = useState<string>('');
+  const [distance, setDistance] = useState<string>('');
+
+  useEffect(() => {
+    if (Object.keys(selectedLog).length) {
+      setGojekEarnings(
+        selectedLog.gojekEarnings ? selectedLog.gojekEarnings.toString() : ''
+      );
+      setGrabEarnings(
+        selectedLog.grabEarnings ? selectedLog.grabEarnings.toString() : ''
+      );
+      setRydeEarnings(
+        selectedLog.rydeEarnings ? selectedLog.rydeEarnings.toString() : ''
+      );
+      setTadaEarnings(
+        selectedLog.tadaEarnings ? selectedLog.tadaEarnings.toString() : ''
+      );
+      setDistance(selectedLog.distance ? selectedLog.distance.toString() : '');
+    }
+  }, [selectedLog]);
+
+  const handleEditLog = () => {
+    const petrolCost =
+      selectedLog.discountedLitrePetrol *
+      ((distance !== '' ? parseFloat(distance) : 0) / 100) *
+      selectedLog.fuelEfficiency;
+    const totalRevenue =
+      (gojekEarnings !== '' ? parseFloat(gojekEarnings) : 0) +
+      (tadaEarnings !== '' ? parseFloat(tadaEarnings) : 0) +
+      (grabEarnings !== '' ? parseFloat(grabEarnings) : 0) +
+      (rydeEarnings !== '' ? parseFloat(rydeEarnings) : 0);
+    const log = {
+      ...selectedLog,
+      gojekEarnings: gojekEarnings !== '' ? parseFloat(gojekEarnings) : 0,
+      grabEarnings: grabEarnings !== '' ? parseFloat(grabEarnings) : 0,
+      rydeEarnings: rydeEarnings !== '' ? parseFloat(rydeEarnings) : 0,
+      tadaEarnings: tadaEarnings !== '' ? parseFloat(tadaEarnings) : 0,
+      distance: distance !== '' ? parseFloat(distance) : 0,
+      petrolCost,
+      totalProfit: totalRevenue - petrolCost,
+      date: selectedLog.date,
+      totalRevenue,
+    };
+    confirmEdit(log);
+    handleClose();
+  };
+  return (
+    <Dialog
+      fullScreen
+      open={openDialog}
+      onClose={() => handleClose()}
+      TransitionComponent={Transition}
+    >
+      <AppBar sx={{ position: 'relative' }}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            onClick={() => handleClose()}
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>
+          <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+            Edit Log
+          </Typography>
+          <Button autoFocus color="inherit" onClick={() => handleEditLog()}>
+            save
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ padding: '30px 20px 20px 20px' }}>
+        <Grid direction="column" spacing={2} container>
+          <Grid item>
+            <TextField
+              label="Date"
+              disabled
+              value={selectedLog.date}
+              fullWidth
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              type="number"
+              id="gojek-earnings"
+              label="Gojek Earnings"
+              value={gojekEarnings}
+              placeholder={'0'}
+              onChange={(e) => setGojekEarnings(e.target.value)}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">$</InputAdornment>
+                ),
+              }}
+              fullWidth
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              type="number"
+              id="grab-earnings"
+              label="Grab Earnings"
+              placeholder={'0'}
+              onChange={(e) => setGrabEarnings(e.target.value)}
+              value={grabEarnings}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">$</InputAdornment>
+                ),
+              }}
+              fullWidth
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              type="number"
+              id="tada-earnings"
+              label="Tada Earnings"
+              placeholder={'0'}
+              onChange={(e) => setTadaEarnings(e.target.value)}
+              value={tadaEarnings}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">$</InputAdornment>
+                ),
+              }}
+              fullWidth
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              type="number"
+              id="ryde-earnings"
+              placeholder={'0'}
+              label="Ryde Earnings"
+              onChange={(e) => setRydeEarnings(e.target.value)}
+              value={rydeEarnings}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">$</InputAdornment>
+                ),
+              }}
+              fullWidth
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              required
+              id="distance"
+              label="Distance"
+              fullWidth
+              type="number"
+              value={distance}
+              placeholder={'0'}
+              onChange={(e) => setDistance(e.target.value)}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">km</InputAdornment>
+                ),
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Box>
+    </Dialog>
+  );
+};
+
+export default EditLog;
--- src/Pages/UserPages/LogListPage/LogListPage.tsx
+++ src/Pages/UserPages/LogListPage/LogListPage.tsx
@@ -51,8 +51,8 @@ import {
   startOfYear,
   endOfYear,
   addYears,
-  isSameDay,
 } from 'date-fns';
+import EditLog from './Components/EditLog';
 
 const defineds = {
   startOfWeek: startOfWeek(new Date()),
@@ -101,11 +101,6 @@ const defaultRanges = {
 };
 
 const LogListPage = () => {
-  const [gojekEarnings, setGojekEarnings] = useState<string>('');
-  const [grabEarnings, setGrabEarnings] = useState<string>('');
-  const [tadaEarnings, setTadaEarnings] = useState<string>('');
-  const [rydeEarnings, setRydeEarnings] = useState<string>('');
-  const [distance, setDistance] = useState<string>('');
   const [hide, setHide] = useState(true);
   const [openDialog, setOpenDialog] = useState(false);
   const [selectedLog, setSelectedLog] = useState<LogInfo>({} as LogInfo);
@@ -399,61 +394,45 @@ const LogListPage = () => {
     // If we are opening the edit dialog - select a log
     if (!openDialog && currentLog) {
       setSelectedLog(currentLog);
-      setGojekEarnings(
-        currentLog.gojekEarnings ? currentLog.gojekEarnings.toString() : ''
-      );
-      setGrabEarnings(
-        currentLog.grabEarnings ? currentLog.grabEarnings.toString() : ''
-      );
-      setRydeEarnings(
-        currentLog.rydeEarnings ? currentLog.rydeEarnings.toString() : ''
-      );
-      setTadaEarnings(
-        currentLog.tadaEarnings ? currentLog.tadaEarnings.toString() : ''
-      );
-      setDistance(currentLog.distance.toString());
-    } else if (!currentLog && openDialog) {
-      // If we are closing the dialog
-      setLogData((state) => {
-        const updatedData = state.map((mappedLog) => {
-          if (mappedLog.id === selectedLog.id) {
-            const petrolCost =
-              mappedLog.discountedLitrePetrol *
-              ((distance !== '' ? parseFloat(distance) : 0) / 100) *
-              selectedLog.fuelEfficiency;
-            const totalRevenue =
-              (gojekEarnings !== '' ? parseFloat(gojekEarnings) : 0) +
-              (tadaEarnings !== '' ? parseFloat(tadaEarnings) : 0) +
-              (grabEarnings !== '' ? parseFloat(grabEarnings) : 0) +
-              (rydeEarnings !== '' ? parseFloat(rydeEarnings) : 0);
-            const log = {
-              ...selectedLog,
-              gojekEarnings:
-                gojekEarnings !== '' ? parseFloat(gojekEarnings) : 0,
-              grabEarnings: grabEarnings !== '' ? parseFloat(grabEarnings) : 0,
-              rydeEarnings: rydeEarnings !== '' ? parseFloat(rydeEarnings) : 0,
-              tadaEarnings: tadaEarnings !== '' ? parseFloat(tadaEarnings) : 0,
-              distance: distance !== '' ? parseFloat(distance) : 0,
-              petrolCost,
-              totalProfit: totalRevenue - petrolCost,
-              date: mappedLog.date,
-              totalRevenue,
-            };
-
-            editEntryOnFirebase(log);
-            return log;
-          } else {
-            return mappedLog;
-          }
-        });
-        return updatedData;
-      });
     }
-
     // set dialog from Open to Close or vice-versa
     setOpenDialog((state) => !state);
   };
 
+  const confirmEditLog = (editedLog: LogInfo) => {
+    // If we are closing the dialog
+    setLogData((state) => {
+      const updatedData = state.map((mappedLog) => {
+        if (mappedLog.id === editedLog.id) {
+          const petrolCost =
+            ((editedLog.discountedLitrePetrol * editedLog.distance) / 100) *
+            editedLog.fuelEfficiency;
+          const totalRevenue =
+            editedLog.gojekEarnings +
+            editedLog.tadaEarnings +
+            editedLog.grabEarnings +
+            editedLog.rydeEarnings;
+          const log = {
+            ...selectedLog,
+            gojekEarnings: editedLog.gojekEarnings,
+            grabEarnings: editedLog.grabEarnings,
+            rydeEarnings: editedLog.rydeEarnings,
+            tadaEarnings: editedLog.tadaEarnings,
+            distance: editedLog.distance,
+            petrolCost,
+            totalProfit: totalRevenue - petrolCost,
+            date: mappedLog.date,
+            totalRevenue,
+          };
+          editEntryOnFirebase(log);
+          return log;
+        } else {
+          return mappedLog;
+        }
+      });
+      return updatedData;
+    });
+  };
   const handleSelectDateRange = (selectedDates: { from: Date; to: Date }) => {
     setDateFilter(selectedDates);
   };
@@ -552,10 +531,6 @@ const LogListPage = () => {
             borderTopLeftRadius: '10px',
             borderTopRightRadius: '10px',
           },
-          // [`& .MuiDataGrid-footerContainer`]: {
-          //   backgroundColor: 'rgba(44, 84, 145, 0.5)',
-          //   borderRadius: '10px',
-          // },
           [`& .${gridClasses.row}.odd`]: {
             backgroundColor: 'rgba(44, 84, 145, 0.1)',
           },
@@ -571,128 +546,12 @@ const LogListPage = () => {
         columns={columns}
         loading={logData.length === 0}
       />
-      <Dialog
-        fullScreen
-        open={openDialog}
-        onClose={() => setOpenDialog(false)}
-        TransitionComponent={Transition}
-      >
-        <AppBar sx={{ position: 'relative' }}>
-          <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              onClick={() => setOpenDialog(false)}
-              aria-label="close"
-            >
-              <CloseIcon />
-            </IconButton>
-            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-              Edit Log
-            </Typography>
-            <Button
-              autoFocus
-              color="inherit"
-              onClick={() => handleEditDialog()}
-            >
-              save
-            </Button>
-          </Toolbar>
-        </AppBar>
-        <Box sx={{ padding: '30px 20px 20px 20px' }}>
-          <Grid direction="column" spacing={2} container>
-            <Grid item>
-              <TextField
-                label="Date"
-                disabled
-                value={selectedLog.date}
-                fullWidth
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                type="number"
-                id="gojek-earnings"
-                label="Gojek Earnings"
-                value={gojekEarnings}
-                placeholder={'0'}
-                onChange={(e) => setGojekEarnings(e.target.value)}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">$</InputAdornment>
-                  ),
-                }}
-                fullWidth
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                type="number"
-                id="grab-earnings"
-                label="Grab Earnings"
-                placeholder={'0'}
-                onChange={(e) => setGrabEarnings(e.target.value)}
-                value={grabEarnings}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">$</InputAdornment>
-                  ),
-                }}
-                fullWidth
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                type="number"
-                id="tada-earnings"
-                label="Tada Earnings"
-                placeholder={'0'}
-                onChange={(e) => setTadaEarnings(e.target.value)}
-                value={tadaEarnings}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">$</InputAdornment>
-                  ),
-                }}
-                fullWidth
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                type="number"
-                id="ryde-earnings"
-                placeholder={'0'}
-                label="Ryde Earnings"
-                onChange={(e) => setRydeEarnings(e.target.value)}
-                value={rydeEarnings}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">$</InputAdornment>
-                  ),
-                }}
-                fullWidth
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                required
-                id="distance"
-                label="Distance"
-                fullWidth
-                type="number"
-                value={distance}
-                placeholder={'0'}
-                onChange={(e) => setDistance(e.target.value)}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">km</InputAdornment>
-                  ),
-                }}
-              />
-            </Grid>
-          </Grid>
-        </Box>
-      </Dialog>
+      <EditLog
+        handleClose={handleEditDialog}
+        confirmEdit={confirmEditLog}
+        openDialog={openDialog}
+        selectedLog={selectedLog}
+      />
     </div>
   );
 };
