ba1f00e891247603ddd767116d897c788711b15b
tyihao <yihaotan1996@gmail.com>
2023-07-31 15:28:54 +0800

    modified log form toggle css

--- src/Pages/UserPages/SubmitLogPage/Components/LogFormToggle.tsx
+++ src/Pages/UserPages/SubmitLogPage/Components/LogFormToggle.tsx
@@ -1,4 +1,20 @@
-import { ToggleButtonGroup, ToggleButton } from '@mui/material';
+import { ToggleButtonGroup, ToggleButton, styled, Paper } from '@mui/material';
+
+const StyledToggleButtonGroup = styled(ToggleButtonGroup)(({ theme }) => ({
+  '& .MuiToggleButtonGroup-grouped': {
+    margin: theme.spacing(0.5),
+    border: 0,
+    '&.Mui-disabled': {
+      border: 0,
+    },
+    '&:not(:first-of-type)': {
+      borderRadius: '10px',
+    },
+    '&:first-of-type': {
+      borderRadius: '10px',
+    },
+  },
+}));
 
 interface ToggleButtonProps {
   logForm: string;
@@ -12,20 +28,38 @@ const LogFormToggle = (props: ToggleButtonProps) => {
     handleLogForm(event.target.value);
   };
 
+  console.log(logForm);
+
   return (
-    <ToggleButtonGroup
-      value={logForm}
-      exclusive
-      onChange={handleOnClick}
-      aria-label="text alignment"
+    <Paper
+      elevation={0}
+      sx={{
+        display: 'flex',
+        border: (theme) => `1px solid ${theme.palette.divider}`,
+        flexWrap: 'wrap',
+        margin: '5px 20px',
+        borderRadius: '10px',
+      }}
     >
-      <ToggleButton value="earnings" aria-label="earnings">
-        Earnings Log
-      </ToggleButton>
-      <ToggleButton value="fuel" aria-label="fuel">
-        Fuel Log
-      </ToggleButton>
-    </ToggleButtonGroup>
+      <StyledToggleButtonGroup
+        value={logForm}
+        exclusive
+        onChange={handleOnClick}
+        aria-label="text alignment"
+        sx={{ width: '100%' }}
+      >
+        <ToggleButton
+          value="earnings"
+          aria-label="earnings"
+          sx={{ width: '50%' }}
+        >
+          Earnings Log
+        </ToggleButton>
+        <ToggleButton value="fuel" aria-label="fuel" sx={{ width: '50%' }}>
+          Fuel Log
+        </ToggleButton>
+      </StyledToggleButtonGroup>
+    </Paper>
   );
 };
 
